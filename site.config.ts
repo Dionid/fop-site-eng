688a0fb97f7a822d3e3933db5faf58d1b5d9cd03
David Shek <ditreyw@gmail.com>
2026-05-09 05:28:13 +0800

    feat: config

--- site.config.ts
+++ site.config.ts
@@ -29,8 +29,8 @@ export default siteConfig({
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
   defaultPageIcon: null,
-  defaultPageCover: "https://fop.davids.sh/images/book_cover.png",
-  defaultPageCoverPosition: 0.5,
+  // defaultPageCover: "https://fop.davids.sh/images/book_cover.png",
+  // defaultPageCoverPosition: 0.5,
 
   // whether or not to enable support for LQIP preview images (optional)
   isPreviewImageSupportEnabled: true,
